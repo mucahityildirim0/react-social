8ec69458bcfb51638f5883acd3c09f2cf071ba3e
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-10-21 19:19:19 +0300

    sidebar component with material icons

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,18 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 import "./home.css";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Topbar />
-    </div>
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
   );
 }
